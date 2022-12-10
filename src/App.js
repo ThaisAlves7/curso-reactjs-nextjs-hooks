c31be03a992a54a4c36b57dea24d6861169a02c7
Thais Alves <thacruzz007@gmail.com>
2022-12-09 23:13:21 -0300

    React Hook - useContext [ Parte 2 ]
    Organização de pastas para situação mais real de Context

--- src/App.js
+++ src/App.js
@@ -1,63 +1,13 @@
-import React, { createContext, useContext, useState } from 'react';
 import './App.css';
 
-const globalState = {
-  title: 'O título do contexto',
-  body: 'O body do contexto',
-  counter: 0,
-};
-
-const GlobalContext = createContext();
-
-// eslint-disable-next-line react/prop-types
-const Div = ({ children }) => {
-  return (
-    <>
-      <H1 />
-      <P />
-    </>
-  );
-};
-
-// eslint-disable-next-line react/prop-types
-const H1 = () => {
-  const theContext = useContext(GlobalContext);
-  const {
-    contextState: { title, counter },
-  } = theContext;
-  return (
-    <h1>
-      {title} {counter}
-    </h1>
-  );
-};
-
-// eslint-disable-next-line react/prop-types
-const P = () => {
-  const theContext = useContext(GlobalContext);
-
-  const {
-    contextState: { body, counter },
-    contextState,
-    setContextState,
-  } = theContext;
-
-  return (
-    <p
-      onClick={() => setContextState((s) => ({ ...s, counter: s.counter + 1 }))}
-    >
-      {body}
-    </p>
-  );
-};
+import { AppContext } from './components/context/AppContext';
+import { Div } from './components/Div';
 
 function App() {
-  const [contextState, setContextState] = useState(globalState);
-
   return (
-    <GlobalContext.Provider value={{ contextState, setContextState }}>
+    <AppContext>
       <Div />
-    </GlobalContext.Provider>
+    </AppContext>
   );
 }
 
---
+++ src/components/context/AppContext/data.js
@@ -0,0 +1,5 @@
+export const globalState = {
+  title: 'O título do contexto alterado',
+  body: 'O paragrafo',
+  counter: 0,
+};
