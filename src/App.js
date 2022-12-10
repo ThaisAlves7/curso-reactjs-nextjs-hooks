c49e331860878bf73bc9047305902223307a9ff6
Thais Alves <thacruzz007@gmail.com>
2022-12-09 21:14:58 -0300

    React Hooks -> useMemo
    O useMemo só recuperará o valor memoizado quando o array receber uma atualização. Esta otimização ajuda a evitar cálculos caros em cada renderização.
    [ useMemo => Memoriza um componente[ ou função de callback ] completamente ]

--- src/App.js
+++ src/App.js
@@ -1,33 +1,59 @@
 import P from 'prop-types';
-import React, { memo, useCallback, useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import './App.css';
 
-// memo => memoriza se o componente foi alterado [Somente atualiza caso o componente seja alterado]
-const Button = memo(function Button({ incrementButton }) {
-  console.log('Filho, renderizou');
-  return <button onClick={() => incrementButton(100)}>+</button>;
-});
+const Post = ({ post }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  console.log('Pai renderizou');
 
-  // Remove a dependência para impedir a renderização de componentes de forma desnecessária
-  const incrementCounter = useCallback((num) => {
-    setCounter((c) => c + num);
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
+
+  // ComponentDidMount => Invocado imediatamente após um componente ser montado
+  useEffect(() => {
+    setTimeout(function () {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((response) => setPosts(response));
+    }, 5000);
   }, []);
 
-  console.log('Pai, renderizou');
   return (
     <div className="App">
-      <p>Teste 3</p>
-      <h1>C1: {counter}</h1>
-      <Button incrementButton={incrementCounter} />
+      <p>
+        <input
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+      {/* useMemo => Memoriza um componente[ ou função de callback] completamente */}
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => <Post key={post.id} post={post} />)
+        );
+      }, [posts])}
+      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
     </div>
   );
 }
 
-Button.propTypes = {
-  incrementButton: P.func,
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
 };
 
 export default App;
