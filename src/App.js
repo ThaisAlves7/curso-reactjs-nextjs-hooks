1e369ee5977ceb49ff287dc549b934369ee0f2cf
Thais Alves <thacruzz007@gmail.com>
2022-12-09 21:46:26 -0300

    React Hook -> useRef
    Pegar uma referencia para alguma coisa no documento ou de algum valor caso necessário.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import P from 'prop-types';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import './App.css';
 
-const Post = ({ post }) => {
+const Post = ({ post, handleClick }) => {
   console.log('Filho renderizou');
   return (
     <div key={post.id} className="post">
-      <h1>{post.title}</h1>
+      <h1 style={{ fontSize: '14px' }} onClick={() => handleClick(post.title)}>
+        {post.title}
+      </h1>
       <p>{post.body}</p>
     </div>
   );
@@ -17,20 +19,35 @@ function App() {
 
   const [posts, setPosts] = useState([]);
   const [value, setValue] = useState('');
+  const input = useRef(null);
+  const contador = useRef(0);
 
   // ComponentDidMount => Invocado imediatamente após um componente ser montado
   useEffect(() => {
-    setTimeout(function () {
-      fetch('https://jsonplaceholder.typicode.com/posts')
-        .then((response) => response.json())
-        .then((response) => setPosts(response));
-    }, 5000);
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((response) => setPosts(response));
   }, []);
+
+  useEffect(() => {
+    input.current.focus();
+    console.log(input.current);
+  }, [value]);
+
+  useEffect(() => {
+    contador.current++;
+  }, []);
+
+  const handleClick = (value) => {
+    setValue(value);
+  };
 
   return (
     <div className="App">
+      <h1>Renderizou: {contador.current}x</h1>
       <p>
         <input
+          ref={input}
           type="search"
           value={value}
           onChange={(e) => setValue(e.target.value)}
@@ -40,7 +57,9 @@ function App() {
       {useMemo(() => {
         return (
           posts.length > 0 &&
-          posts.map((post) => <Post key={post.id} post={post} />)
+          posts.map((post) => (
+            <Post key={post.id} post={post} handleClick={handleClick} />
+          ))
         );
       }, [posts])}
       {posts.length <= 0 && <p>Ainda não existem posts.</p>}
@@ -54,6 +73,7 @@ Post.propTypes = {
     title: P.string,
     body: P.string,
   }),
+  handleClick: P.func,
 };
 
 export default App;
